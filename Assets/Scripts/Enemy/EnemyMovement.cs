c456255d786392712dc50266c19e4c4b2ef63a71
CheekyKnob <b.muhammadfarhan@digipen.edu>
2024-10-22 17:59:49 +0800

    bruh

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
 public class EnemyMovement : MonoBehaviour
 {
     public enum MODE
     {
         PATROL,
-        ATTACKING
+        ATTACKING,
+        ONALERT
     }
 
     EnemyRangeAttackBehaviour rangeAttackBehaviour;
@@ -25,7 +25,11 @@ public class EnemyMovement : MonoBehaviour
     public float coneAngle;  // The angle at the tip of the cone (in degrees)
     public float coneLength;  // The length of the cone
     public float m_Range;
+    public float m_MaxAlertValue;
+    public float m_AlertValue;
     public Transform m_CenterPoint;
+
+    public bool m_PlayerInContact;
 
     public Transform m_Target;
 
@@ -40,6 +44,8 @@ public class EnemyMovement : MonoBehaviour
     private void Update()
     {
         DetectionCone();
+        UpdateBehaviourState();
+
         switch (p_Mode)
         {
             case MODE.PATROL:
@@ -54,8 +60,16 @@ public class EnemyMovement : MonoBehaviour
                     }
                     break;
                 }
-                case MODE.ATTACKING:
+            case MODE.ONALERT:
                 {
+                    transform.LookAt(m_Target.transform);
+                    m_Agent.speed = 0;
+                    break;
+                }
+
+            case MODE.ATTACKING:
+                {
+                    m_Agent.speed = s_MovementSpeed * 1.25f;
                     m_Agent.SetDestination(m_Target.position);
                     if (m_Agent.remainingDistance <= m_Agent.stoppingDistance)
                     {
@@ -100,14 +114,44 @@ public class EnemyMovement : MonoBehaviour
                     {
                         Vector3 playerpos = collider.transform.position;
                         float dist = Vector3.Distance(playerpos, transform.position);
-                        transform.LookAt(collider.transform);
-                        p_Mode = MODE.ATTACKING;
+
+                        if (m_AlertValue < m_MaxAlertValue)
+                            m_AlertValue += Time.deltaTime * 15;
+                        else if (m_AlertValue >= m_MaxAlertValue)
+                            m_AlertValue = m_MaxAlertValue;
+
+                        m_PlayerInContact = true;
+
                         m_Target = collider.transform ;
+                    }
+                }
+                else
+                {
+                    if (m_AlertValue > 0)
+                    {
+                        m_AlertValue -= Time.deltaTime * 5;
                     }
                 }
             }
         }
     }
+
+    void UpdateBehaviourState()
+    {
+        if (m_AlertValue > 50 && m_AlertValue < 95)
+        {
+             p_Mode = MODE.ONALERT;
+        }
+        if (m_AlertValue > 95)
+        {
+            p_Mode = MODE.ATTACKING;
+        }
+        if (m_AlertValue <= 75)
+        {
+            p_Mode = MODE.PATROL;
+        }
+    }
+
     void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
---
+++ Assets/Scripts/Enemy/EnemyUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyUI : MonoBehaviour
+{
+    EnemyMovement s_EnemyMovement;
+
+    [Header("DetectionUI")]
+    public Image ui_DetectionBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        s_EnemyMovement = FindObjectOfType<EnemyMovement>();    
+    }
+
+
+    void UpdateEnemyAlertUI()
+    {
+        ui_DetectionBar.fillAmount = s_EnemyMovement.m_AlertValue / s_EnemyMovement.m_MaxAlertValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateEnemyAlertUI();
+    }
+}
